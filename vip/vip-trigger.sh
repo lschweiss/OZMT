cd635a88ac8feb30479c4a2f0126ca1d20d92a89
Chip Schweiss <schweissl@mir.wustl.edu>
2015-10-07 08:39:34 -0500

    Handle zfs_cache location reporting.

--- vip/vip-trigger.sh
+++ vip/vip-trigger.sh
@@ -416,7 +416,7 @@ process_vip () {
                 debug "No source reference for dataset ${dataset_name}"
                 # Get the zfs folder for the dataset
                 zfs_folder=`cat ${pool}/zfs_tools/var/replication/datasets/${dataset_name}`
-                replication=`zfs_cache get $zfs_replication_property ${pool}/${zfs_folder}`
+                replication=`zfs_cache get $zfs_replication_property ${pool}/${zfs_folder} 3>/dev/null`
                 if [ "$replication" == 'on' ]; then
                     error "Replication is on, no source set.  Deactivating vip."
                     deactivate_vip "$vIP"
